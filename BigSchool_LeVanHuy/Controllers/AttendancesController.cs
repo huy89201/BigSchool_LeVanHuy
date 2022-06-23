f6f2b3d5a28162088c766c02e201959c0fedaf7a
Huy Le <huy.le@techupcorp.com>
2022-06-23 17:10:00 +0700

    finish

---
+++ BigSchool_LeVanHuy/Controllers/AttendancesController.cs
@@ -0,0 +1,47 @@
+﻿using BigSchool_LeVanHuy.DTO;
+using BigSchool_LeVanHuy.Models;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+
+
+namespace BigSchool_LeVanHuy.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto AttendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == AttendanceDto.CourseId))
+                return BadRequest("The Attendance already exists");
+
+
+            var attendance = new Attendance
+            {
+                CourseId = AttendanceDto.CourseId,
+                AttendeeId = userId
+            };
+
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+
+    }
+
+}
